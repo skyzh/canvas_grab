99c030a02c2d7d5bf123e706256f2642100ed6f5
Alex Chi <iskyzh@gmail.com>
2020-03-26 19:56:00 +0800

    [code] re-uploaded files with same name will now be downloaded

--- main.py
+++ main.py
@@ -197,7 +197,6 @@ def check_download_rule(file, path, json_key) -> (bool, str, bool):
             if checkpoint[json_key]["id"] != file.id:
                 print(
                     f"    {Fore.YELLOW}Duplicated files detected. ({file.display_name}){Style.RESET_ALL}")
-                return (False, "files with duplicated path", update_flag)
 
     return (True, "", update_flag)
 
