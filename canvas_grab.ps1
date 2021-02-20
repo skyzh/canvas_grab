b60f269a8df81de7422e84b325e14826de1dab5b
Alex Chi <iskyzh@gmail.com>
2021-02-20 16:05:33 +0800

    docs: fix quick-start docs (#114)

--- canvas_grab.ps1
+++ canvas_grab.ps1
@@ -2,22 +2,22 @@
 $ErrorActionPreference = "Stop"
 
 echo "Check Python installation..."
-python3 --version
+python --version
 
 echo "Check virtual environment..."
 
 if (-Not(Test-Path -Path venv)) {
     echo "Create virtual environment..."
-    python3 -m venv venv
+    python -m venv venv
     echo "Activate virtual environment..."
-    . ./venv/bin/Activate.ps1
+    . ./venv/Scripts/Activate.ps1
     echo "Install dependencies with SJTUG mirror..."
     python -m pip install --upgrade pip -i https://mirrors.sjtug.sjtu.edu.cn/pypi/web/simple
     python -m pip install -r requirements.txt -i https://mirrors.sjtug.sjtu.edu.cn/pypi/web/simple
     python -m pip install -r requirements.windows.txt -i https://mirrors.sjtug.sjtu.edu.cn/pypi/web/simple
 } else {
     echo "Activate virtual environment..."
-    . ./venv/bin/Activate.ps1
+    . ./venv/Scripts/Activate.ps1
 }
 
-python main.py $@
+python main.py $args
