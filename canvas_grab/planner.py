2514eb36761829d0c17976ce1bf0eb101b5eff71
Alex Chi <iskyzh@gmail.com>
2021-02-26 10:15:20 +0800

    code: show ignored files when transfer (#123)

--- canvas_grab/planner.py
+++ canvas_grab/planner.py
@@ -2,11 +2,15 @@ class Planner(object):
     def __init__(self, remove_local_file):
         self.remove_local_file = remove_local_file
 
-    def plan(self, snapshot_from, snapshot_to):
+    def plan(self, snapshot_from, snapshot_to, file_filter):
+        snapshot_from_filter = file_filter.filter_files(snapshot_from)
+
         plans = []
         # Add and update files
         for key, from_item in snapshot_from.items():
-            if key not in snapshot_to:
+            if key not in snapshot_from_filter:
+                plans.append(('ignore', key, from_item))
+            elif key not in snapshot_to:
                 plans.append(('add', key, from_item))
             else:
                 to_item = snapshot_to[key]
@@ -14,6 +18,6 @@ class Planner(object):
                     plans.append(('update', key, from_item))
         # Remove files
         for key, to_item in snapshot_to.items():
-            if key not in snapshot_from:
+            if key not in snapshot_from_filter:
                 plans.append(('delete', key, to_item))
         return plans
