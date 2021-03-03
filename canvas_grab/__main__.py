872de4693d90f30b0f6f4de69275352735b53617
Victrid <weihao.jiang@sjtu.edu.cn>
2021-03-03 15:14:04 +0800

    Update English Syntax

--- canvas_grab/__main__.py
+++ canvas_grab/__main__.py
@@ -24,34 +24,44 @@ def get_options():
         print('--------------------')
 
     # Argument Parser initiation
+
     parser = argparse.ArgumentParser(
-        description='Grab all files on Canvas LMS to local directory.')
+        description='Grab all files on Canvas LMS to local directory.',
+        epilog="Configuration file variables specified with program arguments will override the "
+        "original settings at runtime, but will not be written to the original configuration file. "
+        "If you specify a configuration file with the --config-file argument when you configure it, "
+        "it will be overwritten with the new content.")
 
     # Interactive
     interactive_group = parser.add_mutually_exclusive_group()
     interactive_group.add_argument("-i", "--interactive", dest="interactive", action="store_true",
-                                   default=True, help="Set the program to run in interactive mode (default action)")
-    interactive_group.add_argument("-I", "--non-interactive", "--no-input", dest="interactive", action="store_false", default=True,
-                                   help="Set the program to run in non-interactive mode. This can be used to exit immediately in case of profile corruption without getting stuck with the input.")
+                                   default=True,
+                                   help="Set the program to run in interactive mode (default action)")
+    interactive_group.add_argument("-I", "--non-interactive", "--no-input", dest="interactive",
+                                   action="store_false", default=True,
+                                   help="Set the program to run in non-interactive mode. This can be "
+                                   "used to exit immediately in case of profile corruption without "
+                                   "getting stuck with the input.")
 
     # Reconfiguration
-    parser.add_argument("-r", "--reconfigure", "--configure", dest="reconfigure", help="Reconfigure the tool.",
-                        action="store_true")
+    parser.add_argument("-r", "--reconfigure", "--configure", dest="reconfigure",
+                        help="Reconfigure the tool.", action="store_true")
 
     # Location Specification
     parser.add_argument("-o", "--download-folder", "--output",
-                        dest="download", help="Set the download folder.")
+                        dest="download", help="Specify alternative download folder.")
     parser.add_argument("-c", "--config-file", dest="config_file", default="config.toml",
-                        help="Specify the configuration file. The configurations are loaded in this order: arguments, file specified here, and the default config file config.toml.")
+                        help="Specify alternative configuration file.")
 
     # Generic Options
     # TODO quiet mode
-    # parser.add_argument("-q", "--quiet",dest="quiet", help="Start the program in quiet mode. Only errors will be printed.",
-    #                 action="store_true")
+    # parser.add_argument("-q", "--quiet", dest="quiet", help="Start the program in quiet mode. "
+    #                     "Only errors will be printed.", action="store_true")
     parser.add_argument("--version", action="version",
                         version=canvas_grab.__version__)
     parser.add_argument("-k", "--keep-version", "--no-update", dest="noupdate", action="store_true",
-                        default=False, help="Skip update checking. This will be helpful without a stable network connection and prevent reconfiguration.")
+                        default=False, help="Skip update checking. This will be helpful without "
+                        "a stable network connection and prevent reconfiguration.")
 
     args = parser.parse_args()
 
@@ -79,7 +89,7 @@ def get_options():
         except KeyboardInterrupt:
             print("User canceled the configuration process")
             return
-        Path(args.config_file).write_text(
+        Path(config_file).write_text(
             toml.dumps(config.to_config()), encoding='utf8')
     if args.download:
         config.download_folder = args.download
