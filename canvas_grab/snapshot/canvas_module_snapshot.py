6a40aad5692c3c89a728fbacdc172f3619a0a299
Alex Chi <iskyzh@gmail.com>
2021-03-03 23:30:21 +0800

    code: fix module mode indent

--- canvas_grab/snapshot/canvas_module_snapshot.py
+++ canvas_grab/snapshot/canvas_module_snapshot.py
@@ -49,53 +49,53 @@ class CanvasModuleSnapshot(Snapshot):
              dict: snapshot of Canvas in `SnapshotFile` or `SnapshotLink` type.
          """
         course = self.course
-         request_batcher = RequestBatcher(course)
-          accessed_files = []
+        request_batcher = RequestBatcher(course)
+        accessed_files = []
 
-           for _, module in (request_batcher.get_modules() or {}).items():
-                # replace invalid characters in name
-                name = re.sub(file_regex, "_", module.name)
-                # consolidate spaces
-                name = " ".join(name.split())
+        for _, module in (request_batcher.get_modules() or {}).items():
+            # replace invalid characters in name
+            name = re.sub(file_regex, "_", module.name)
+            # consolidate spaces
+            name = " ".join(name.split())
 
-                # get module index
-                idx = str(module.position)
+            # get module index
+            idx = str(module.position)
 
-                # folder format
-                module_name = f'{idx} {name}'
+            # folder format
+            module_name = f'{idx} {name}'
 
-                module_item_count = module.items_count
-                print(
-                    f'  Module {colored(module_name, "cyan")} ({module_item_count} items)')
+            module_item_count = module.items_count
+            print(
+                f'  Module {colored(module_name, "cyan")} ({module_item_count} items)')
 
-                for item in module.get_module_items():
-                    if item.type == 'File':
-                        file_id = item.content_id
-                        snapshot_file = from_canvas_file(
-                            request_batcher.get_file(file_id))
-                        accessed_files.append(file_id)
-                        filename = f'{module_name}/{snapshot_file.name}'
-                        self.add_to_snapshot(filename, snapshot_file)
-                    if self.with_link:
-                        if item.type == 'ExternalUrl' or item.type == 'Page':
-                            key = f'{module_name}/{item.title}.html'
-                            value = SnapshotLink(
-                                item.title, item.html_url, module_name)
-                            self.add_to_snapshot(key, value)
+            for item in module.get_module_items():
+                if item.type == 'File':
+                    file_id = item.content_id
+                    snapshot_file = from_canvas_file(
+                        request_batcher.get_file(file_id))
+                    accessed_files.append(file_id)
+                    filename = f'{module_name}/{snapshot_file.name}'
+                    self.add_to_snapshot(filename, snapshot_file)
+                if self.with_link:
+                    if item.type == 'ExternalUrl' or item.type == 'Page':
+                        key = f'{module_name}/{item.title}.html'
+                        value = SnapshotLink(
+                            item.title, item.html_url, module_name)
+                        self.add_to_snapshot(key, value)
 
-            files = request_batcher.get_files()
-            if files:
-                unmoduled_files = 0
-                for file_id, file in files.items():
-                    if file_id not in accessed_files:
-                        snapshot_file = from_canvas_file(file)
-                        filename = f'unmoduled/{snapshot_file.name}'
-                        self.add_to_snapshot(filename, snapshot_file)
-                        unmoduled_files += 1
-                print(
-                    f'  {colored("Unmoduled files", "cyan")} ({unmoduled_files} items)')
+        files = request_batcher.get_files()
+        if files:
+            unmoduled_files = 0
+            for file_id, file in files.items():
+                if file_id not in accessed_files:
+                    snapshot_file = from_canvas_file(file)
+                    filename = f'unmoduled/{snapshot_file.name}'
+                    self.add_to_snapshot(filename, snapshot_file)
+                    unmoduled_files += 1
+            print(
+                f'  {colored("Unmoduled files", "cyan")} ({unmoduled_files} items)')
 
-            return self.snapshot
+        return self.snapshot
 
     def get_snapshot(self):
         """Get the previously-taken snapshot
